2188eb732ebbf9f76def7144d3425311e5704b4a
honse4 <127538575+honse4@users.noreply.github.com>
2024-11-13 16:32:09 +0200

    Update firebase.ts
    Editing out keys

--- dropbox-clone/firebase.ts
+++ dropbox-clone/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXPVVJR_c0Qt--NgHrub0jRBa_daKTHqc",
-    authDomain: "dropbox-clone-22ccd.firebaseapp.com",
-    projectId: "dropbox-clone-22ccd",
-    storageBucket: "dropbox-clone-22ccd.appspot.com",
-    messagingSenderId: "275876310513",
-    appId: "1:275876310513:web:1c340bdcbce1cc37392850"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   };
 
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
